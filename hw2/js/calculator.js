5bdda658043b158d59765b2ccdf145591e541322
Kimberly Lim <klimjinx@andrew.cmu.edu>
2018-01-26 13:43:47 -0500

    added operator function with first test pass

--- hw2/js/calculator.js
+++ hw2/js/calculator.js
@@ -1,17 +1,60 @@
-var newValue = 0;
-var prevValue = 0;
 
 var OpTypes = {
-  PLUS,
-  MINUS,
-  DIVIDE,
-  TIMES,
-  EQUALS
+  PLUS: '+',
+  MINUS: '-',
+  DIVIDE: '/',
+  TIMES: '*',
+  EQUALS: '='
 };
 
-function digitPress(digit) {
+var newValue = 0;
+var prevValue = 0;
+var prevOp = OpTypes.PLUS;
+var lastWasOp = false;
+
+function updateDisplay(value) {
     var display   = document.getElementById("display");
-        
-    newValue = newValue * 10 + digit;
-    display.innerHTML = newValue;
+    display.innerHTML = value;
+}
+
+function digitPress(digit) {
+    if (lastWasOp) {
+        newValue = digit;
+    } else {
+        newValue = newValue * 10 + digit;
+    }
+    updateDisplay(newValue);
+    lastWasOp = false;
+}
+
+function evaluate() {
+    switch (prevOp) {
+        case OpTypes.PLUS:
+            prevValue = newValue + prevValue;
+            break;
+
+        case OpTypes.MINUS:
+            prevValue = prevValue - newValue;
+            break;
+
+        case OpTypes.DIVIDE:
+            prevValue = prevValue / newValue;
+            break;
+
+        case OpTypes.TIMES:
+            prevValue = newValue * prevValue;
+            break;
+        case OpTypes.EQUALS:
+            break;
+        default:
+            break;
+    }
+}
+
+function operatorPress(operator) {
+    evaluate();
+    updateDisplay(prevValue);
+    prevOp = operator;
+    newValue = 0;
+    lastWasOp = true;
 }
